5d44a17f38948c62a34da90ec071d3acecd4689d
geronimo304 <geronimonegreira06@gmail.com>
2025-02-17 18:19:50 +0000

    trabajo shell

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,31 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
-*main - return point
-*Description:generate a number random
-*return: Alweys 0 (Success)
-*/
-	int main(void)
-{	
-	
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
 
-	/*main code*/
-	srand(time(NULL));
-	int n = rand(); RAND_MAX / 2;
-	
-	if (n > 0) {
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+    /* Your code goes here */
+	if (n > 0)
+	{
 	printf("%d is positive\n", n);
 	}
-	else if (n == 0) {
+	else if (n == 0)
+	{
 	printf("%d is zero\n", n);
 	}
-	else {
+	else
+	{
 	printf("%d is negative\n", n);
 	}
+
 	return (0);
-}
+	}
