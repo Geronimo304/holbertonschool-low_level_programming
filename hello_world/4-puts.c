ad7f19ecc78c8888d50b49f1be4d0fd66db9cc9f
geronimo304 <geronimonegreira06@gmail.com>
2025-02-17 12:24:48 +0000

    trabajo shell

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/**
-*	main - Entry point
-*	
-*
-*	Return: Always 0
-*/
 
 
 int main(void)
@@ -13,17 +7,5 @@ int main(void)
 
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
