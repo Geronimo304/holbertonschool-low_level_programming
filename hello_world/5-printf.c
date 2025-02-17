1bdfa79896cd9a253ec79e4b87b8a557c054d8a5
geronimo304 <geronimonegreira06@gmail.com>
2025-02-17 13:16:14 +0000

    trabajo shell

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,15 +2,12 @@
 
 /**
  *main - Entry point
- *
  *Description comando que imprime with proper grammar, but the outcome is a piece of art, 
- *
- *return 0 (Succes)
- *
- *
+ *return:0
  */
+	
 	int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
