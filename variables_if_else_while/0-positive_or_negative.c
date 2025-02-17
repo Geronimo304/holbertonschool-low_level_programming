fd46e44d9e97f1cd36dafde8454cdf528d17e43a
geronimo304 <geronimonegreira06@gmail.com>
2025-02-17 16:12:22 +0000

    trabajo shell

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,12 @@
 	int main(void)
 {	
 	int n = rand();
-	if (n > 0){
+	if (n > 0) {
 	        printf("%d is positive\n", n);
-	
-	}else if (n == 0){
+	} else if (n == 0) {
 	        printf("%d is zero\n", n);
-	
-	}else (n < 0){
-		printf("%n is negative\n", n);
+	} else {
+		printf("%d is negative\n", n);
 	}
-	return 0;
+	return (0);
 }
