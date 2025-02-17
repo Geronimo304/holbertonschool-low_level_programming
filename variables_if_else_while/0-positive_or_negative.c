2df1f8e36d879e3024203f0ed5f22463d1bc7b6c
geronimo304 <geronimonegreira06@gmail.com>
2025-02-17 15:17:27 +0000

    trabajo shell

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *int - Main Point
+ *Description:generate a random number.
+ *return (0);
+ */
+	int main(void)
+{
+	int n = rand();
+	printf("Numero aleatorio:%d\n",numero);
+	return (0);
+}
