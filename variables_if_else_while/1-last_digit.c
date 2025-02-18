250cab989e247d34e460666643b562133690e73f
geronimo304 <geronimonegreira06@gmail.com>
2025-02-18 12:43:11 +0000

    trabajo shell

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 		printf("Last digit of %d is %d ", n, last_digit);
 
-if last_digit > 5
+if (last_digit > 5)
 {
 	printf("and is greater than 5\n");
 }
@@ -29,5 +29,4 @@ else
 	printf("and is less than 6 and not 0\n");
 }
 return (0);
-
 }
