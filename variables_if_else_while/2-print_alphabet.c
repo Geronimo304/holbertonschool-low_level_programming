0078b0e69c4a95aaa09f531b3ffe5197aa529366
geronimo304 <geronimonegreira06@gmail.com>
2025-02-18 13:05:07 +0000

    trabajo shell

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ab;
+
+	for (ab = 'a'; ab <= 'z';ab++)
+		putchar (ab);
+
+	putchar ('\n');
+	return (0);
+}
