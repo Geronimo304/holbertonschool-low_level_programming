d6ffa7e314bdf3d8100c32ff44b723390d461e7c
geronimo304 <geronimonegreira06@gmail.com>
2025-02-18 13:58:29 +0000

    trabajo shell

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ab;
+
+	for (ab = 'a'; ab <= 'z' ; ab++)
+		if (ab != 'q' && ab != 'e')
+		putchar(ab);
+
+	putchar ('\n');
+	return (0);
+}
