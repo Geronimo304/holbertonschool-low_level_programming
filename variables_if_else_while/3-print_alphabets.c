ecd13d94826a4084fc03b3a4814d1004c9a58922
geronimo304 <geronimonegreira06@gmail.com>
2025-02-18 13:30:15 +0000

    trabajo shell

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ab;
+
+	for (ab = 'a'; ab <= 'z';ab++)
+		putchar(ab);
+	
+	for (ab = 'A'; ab  <= 'Z';ab++)
+		putchar(ab);
+
+	putchar ('\n');
+	return (0);
+}
