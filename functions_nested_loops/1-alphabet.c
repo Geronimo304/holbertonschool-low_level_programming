ae3cd75514ca7f8ca2df3621eafbaccf1ca9632a
geronimo304 <geronimonegreira06@gmail.com>
2025-02-19 13:57:31 +0000

    trabajo shell

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,18 +2,19 @@
 
 /**
  * main - Entry Point
- * Description
- * Return: 0 
+ * Description: Print the alphabet in lowercase
+ * Return: 0
  */
 
 void print_alphabet(void)
 {
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-	int i= 0;
+	int i = 0;
 
 	while (alphabet[i] != '\0')
 	{
 		_putchar(alphabet[i]);
 		i++;
 	}
+return (0);
 }
