05aa15a57334155721d92923dc1d8b8277d849b8
geronimo304 <geronimonegreira06@gmail.com>
2025-02-19 16:58:26 +0000

    trabajo shell

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry Point
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return(1);
+	else
+		return(0);
+
+}
