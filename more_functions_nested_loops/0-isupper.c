6e827eacaabd697d8649090383f3c83aa52997cb
geronimo304 <geronimonegreira06@gmail.com>
2025-02-25 20:36:50 +0000

    trabajo shell

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
-/*
- *
- *
- *
- *
+/**
+ * _isupper - Entry Point
+ * @c: returns 1 if uppercase.
+ * Return: 0 Always
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
