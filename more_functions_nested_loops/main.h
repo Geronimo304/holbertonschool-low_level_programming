5485f80cc0478daa33da040912305c7d08d35de8
geronimo304 <geronimonegreira06@gmail.com>
2025-02-26 21:36:02 +0000

    trabajo shell

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,11 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_line(int n);
 void more_numbers(void);
 void print_most_numbers(void);
-int _putchar(char c);
 void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
+int _putchar(char c);
 #endif
