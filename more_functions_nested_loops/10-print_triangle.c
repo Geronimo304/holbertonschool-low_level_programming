a463a4cc88de2fe2157e85a17a60f6aa2d69ed4e
geronimo304 <geronimonegreira06@gmail.com>
2025-02-27 12:56:23 +0000

    tyrabajo shell

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry Point
+ * @size: triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (spaces = size - i; spaces > 0; spaces--)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
