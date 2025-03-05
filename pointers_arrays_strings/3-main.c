32d562ba06b2d054e840c4e134ad863e2b797a60
geronimo304 <geronimonegreira06@gmail.com>
2025-03-05 20:31:09 +0000

    trabajo shell

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -2,17 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code
  *
- * Return: Always 0.
+ *
+ *
  */
-int main(void)
+unsigned int _strspn(char *s, char *accept)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+	int i;
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
+	
 }
