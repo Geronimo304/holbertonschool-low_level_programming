66f0f08f501323900b18628fb8a7b62635d06e68
geronimo304 <geronimonegreira06@gmail.com>
2025-03-07 19:36:40 +0000

    trabajo shell

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Entry Point
+ * @a: puntero que contiene el contenido del trabajo
+ * @size: memora del puntero
+ */
+void print_diagsums(int *a, int size)
+{
+	int suma1, suma2;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		suma1 += a[i * size + i];
+		suma2 += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", suma1, suma2);
+}
