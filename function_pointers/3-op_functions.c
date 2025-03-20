4fd02de89f6a5591e6eef16dd7a3fc17cbc295ac
geronimo304 <geronimonegreira06@gmail.com>
2025-03-20 17:21:01 +0000

    trabajo shell

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - suma
+ * op_sub - resta
+ * op_mul - multiplica
+ * op_div - divide
+ * op_mod - resto de la division
+ * @a: num1
+ * @b: num2
+ * Return: retorna la cuenta
+ */
+int op_add(int a, int b)
+{
+	return(a + b);
+}
+int op_sub(int a, int b)
+{
+	return(a - b);
+}
+int op_mul(int a, int b)
+{
+	return(a * b);
+}
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		exit(100);
+	}
+	return(a / b);
+}
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		exit(100);
+	}
+	return(a % b);
+}
