fbf535f5ced4c62e6053581c401cef54ac54bd31
geronimo304 <geronimonegreira06@gmail.com>
2025-03-21 18:21:59 +0000

    division

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -12,32 +12,33 @@
  * @b: num2
  * Return: retorna la cuenta
  */
-int op_add(int a, int b)
-{
-	return(a + b);
-}
-int op_sub(int a, int b)
-{
-	return(a - b);
-}
-int op_mul(int a, int b)
-{
-	return(a * b);
-}
-int op_div(int a, int b)
-{
-	if (b == 0)
+	int op_add(int a, int b)
 	{
-		printf("Error\n");
-		exit(98);
+		return(a + b);
 	}
-	return(a / b);
-}
-int op_mod(int a, int b)
-{
-	if (b == 0)
+	int op_sub(int a, int b)
 	{
-		exit(100);
+		return(a - b);
 	}
-	return(a % b);
+	int op_mul(int a, int b)
+	{
+		return(a * b);
+	}	
+	int op_div(int a, int b)
+	{
+		if (b == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		return(a / b);
+	}
+	int op_mod(int a, int b)
+	{
+		if (b == 0)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		return(a % b);
 }
