d3a2289ecaa24d3c1170ec2dcf1b6d2dd88b8a09
geronimo304 <geronimonegreira06@gmail.com>
2025-04-05 21:32:46 +0000

    trabajo 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,23 +11,35 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	char *new_str = 0;
+	unsigned int len = 0;
 
-	if (!head || !str)
+
+	if (head == NULL || str == NULL)
+	{
 		return (NULL);
+	}
+	new_str = strdup(str);
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
-	if (!new_node)
-		return (NULL);
 
-	new_node->str = strdup(str);
-	if (!new_node->str)
+	if (new_str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->str = new_str;
+	new_node->len = len;
 	new_node->next = *head;
+
 	*head = new_node;
 
 	return (new_node);
