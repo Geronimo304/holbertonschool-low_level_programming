fcdbf90f5d7676a09c437b3f024fa48405e4eb59
geronimo304 <geronimonegreira06@gmail.com>
2025-04-05 20:39:12 +0000

    trabajo 1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - Devuelve el número d en una lista enlazada tipo list_t.
+ * list_len - Devuelve el número i en una lista enlazada tipo list_t.
  * @h: Puntero al inicio de la lista.
  *
  * Return: El número de elementos en la lista.
@@ -16,5 +16,5 @@ size_t list_len(const list_t *h)
 		h = h->next;
 	}
 
-	return count;
+	return (ount)
 }
