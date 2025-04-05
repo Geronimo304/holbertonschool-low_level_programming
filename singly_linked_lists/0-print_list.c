09987a70053a135b101fdb50708c914ab94a270e
geronimo304 <geronimonegreira06@gmail.com>
2025-04-05 20:08:14 +0000

    trabajo 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_list - imprime la lista
+ * @h: puntero hacia la lista
+ * Return: retorna el numero de nodos de la lista
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
